6c3514440896c64aed7e085e00905a6ebecdc770
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-02-06 22:24:32 -0700

    fix model index and add homepage handlebar and route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,25 @@
 const router = require('express').Router();
 
+// GET homepage
+router.get('/', async (req, res) => {
+    try {
+      res.render('homepage', {
+        loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+// GET login page
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
