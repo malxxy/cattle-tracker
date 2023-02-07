11981fbd3754db6a7664b85a4c61f08c0512f886
slexicon <lexiscott0101@gmail.com>
2023-02-06 21:20:31 -0800

    update seed with ownerID

--- models/cattle.js
+++ models/cattle.js
@@ -1,18 +1,1 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-
-class Cattle extends Model {}
-
-Cattle.init(
-  {
-
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'cattle',
-  }
-);
-
-module.exports = Post;
+const { Model, Datatypes } = require('sequilize');
