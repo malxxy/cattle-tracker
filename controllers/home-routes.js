7a3e4ef85ebaa98669acec96930473a37754b50c
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-02-09 22:10:31 -0700

    save user id, rancnum, and name in session info

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,32 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // GET homepage
-router.get('/', async (req, res) => {
-  console.log(req.session.loggedIn)
-    try {
-      res.render('homepage', {
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-
-console.log(posts)
-router.get('/login', async (req, res) => {
-  {posts,user_id,cattledata}; 
-  //catch (err) {
-    //console.log(err);
-    //res.status(500).json(err);
-  //}
-
-
-  // GET Dashboard Page
-router.get('/dashboard', async (req, res) => {
+router.get("/", async (req, res) => {
+  console.log(req.session.loggedIn);
   try {
-    res.render('dashboard', {
+    res.render("homepage", {
       loggedIn: req.session.loggedIn,
+      userid: req.session.userId,
+      name: req.session.name,
+      ranchNum: req.session.ranchNum,
+      ranch: req.session.ranch
     });
   } catch (err) {
     console.log(err);
@@ -34,16 +17,43 @@ router.get('/dashboard', async (req, res) => {
   }
 });
 
-  // GET Cattle Page
-  router.get('/cattle', async (req, res) => {
-    try {
-      res.render('cattle', {
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+// console.log(posts)
+// router.get('/login', async (req, res) => {
+//   {posts,user_id,cattledata};
+//   //catch (err) {
+//     //console.log(err);
+//     //res.status(500).json(err);
+//   //}
+
+// GET Dashboard Page
+router.get("/dashboard", async (req, res) => {
+  try {
+    console.log(req.session.userId)
+
+    
+    res.render("dashboard", {
+      loggedIn: req.session.loggedIn,
+      userId: req.session.userId,
+      name: req.session.name,
+      ranchNum: req.session.ranchNum,
+      ranch: req.session.ranch
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET Cattle Page
+router.get("/cattle", async (req, res) => {
+  try {
+    res.render("cattle", {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
