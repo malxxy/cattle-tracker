53b7175e70f9abc61da5c78ee42b4ec7b7ad2857
Maislinn <maislinn.helfer@gmail.com>
2023-02-09 19:51:17 -0800

    Added partials, adjusted routes, deleted the routes folder, adjusted post-routes.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,23 @@
 const router = require('express').Router();
-// const { Cattle, User } = require('../../models');
+const { Post, User } = require('../../models');
+
+// New Post
+
+router.post('/', async (req, res) => {
+    console.log("New Post")
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+            username: req.session.username
+        });
+        console.log(newPost)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 
 
 module.exports = router;
