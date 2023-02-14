0adae9af7a5e0bc1c14fe9ef2e73f34c0ce79525
malxxy <maliayraguen@gmail.com>
2023-02-13 16:20:28 -0800

    need backend for handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,6 @@
 async function showLivestock() {
-    const response = await fetch('/api/cattle', {
-      method: 'POST',
+    const response = await fetch('/api/cattle-routes', {
+      method: 'GET',
       headers: { 'Cattle': 'application/json' },
     });
   
@@ -11,20 +11,21 @@ async function showLivestock() {
     }
   }
 
-  async function addLivestock() {
-    const response = await fetch('/api/cattle', {
-      method: 'POST',
-      headers: { 'Cattle:' 'application/json' },
-    });
+// Need handlebars done for add livestock page
+//   async function addLivestock() {
+//     const response = await fetch('/api/cattle', {
+//       method: 'POST',
+//       headers: { 'Cattle': 'application/json' },
+//     });
   
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to load add livestock page');
-    }
-  }
+//     if (response.ok) {
+//       document.location.replace('/');
+//     } else {
+//       alert('Failed to load add livestock page');
+//     }
+//   }
   
 // Upon clicking My Livestock, take to My Livestock partial handlebars page
 document.querySelector('#my-livestock-btn').addEventListener('click', showLivestock);
 // Upon clicking add Livestock, take to add Livestock handlebars page
-document.querySelector('add-livestock-btn').addEventListener('click',addLivestock);
+document.querySelector('#add-livestock-btn').addEventListener('click',addLivestock);
