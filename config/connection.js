8c824e88788652489c1634a850ac6b62ba35e88e
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-02-15 16:26:16 -0700

    change connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  DB_NAME='heroku_cda25d35d22a229',
+  DB_PASSWORD='6e246bbc',
+  DB_USER='b14d129264f667',
+
+
   {
-    host: '127.0.0.1',
+    host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     port: 3306,
   }
