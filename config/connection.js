495beb1578acf7998476695e9f0243480cab24d3
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-02-15 16:31:42 -0700

    change connection format

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
   // process.env.DB_NAME,
   // process.env.DB_USER,
   // process.env.DB_PASSWORD,
-  DB_NAME='heroku_cda25d35d22a229',
-  DB_PASSWORD='6e246bbc',
-  DB_USER='b14d129264f667',
-
+  
+  "heroku_cda25d35d22a229",
+  "b14d129264f667",
+  "6e246bbc",
 
   {
-    host: 'us-cdbr-east-06.cleardb.net',
-    dialect: 'mysql',
+    host: "us-cdbr-east-06.cleardb.net",
+    dialect: "mysql",
     port: 3306,
   }
 );
